de3a4054aabe59aecbab2302ba69f232003b916e
Bori-github <qhflrnfl4324@gmail.com>
2026-06-25 19:48:56 +0900

    refactor(station): entity public API export 범위 축소
    - stationQueryKeys, Station 타입 export 제거
    - 검색 feature에서 사용하는 StationSearchResult만 노출

--- apps/web/src/entities/station/index.ts
+++ apps/web/src/entities/station/index.ts
@@ -1,3 +1,2 @@
 export { getStationsByNameQueryOptions } from './model/queries';
-export { stationQueryKeys } from './model/queryKeys';
-export type { Station, StationSearchResult } from './model/types';
+export type { StationSearchResult } from './model/types';
